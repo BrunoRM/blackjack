4d39120355854acec2d69c65b6d4f771b8b55f7c
Bruno Rodrigues Marciano <bruno-9237@hotmail.com>
2018-11-09 14:38:11 -0200

    Inicial

---
+++ main.py
@@ -0,0 +1,47 @@
+# cartas de 2-10 valem o valor do número na face da carta.
+# Cartas numeradas valem o número correspondente indicado 
+# na carta. As cartas de rosto (aquelas com imagens) 
+# valem 10, exceto para o Ás, que vale 1 ou 11. Uma 
+# imagem combinada com um Ás é Blackjack (um valor de 21).
+
+# TODO: Usar emojis para os naipes das cartas
+# Para usar os emojis de cartas:
+# Espada :spades:
+# Copas  :hearts:
+# Paus   :clubs:
+# Ouro   :diamonds:
+
+import emoji
+import copy
+
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __str__(self):
+        return f'{self.valor}{self.naipe}'
+
+naipes = (
+    emoji.emojize(':spades:', use_aliases=True), 
+    emoji.emojize(':hearts:', use_aliases=True),
+    emoji.emojize(':clubs:', use_aliases=True),
+    emoji.emojize(':diamonds:', use_aliases=True)
+)
+
+cartas = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+
+def criar_baralho():
+    baralho = []
+    for valor in cartas:
+        for naipe in naipes:
+            carta = Carta(valor, naipe)
+            baralho.append(carta)
+
+    return baralho
+
+baralho = criar_baralho()
+
+print(baralho)
+for carta in baralho:
+    print(carta)
