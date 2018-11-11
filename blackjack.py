2be88b4a8907b2b361c4045184d5b4bf04331c97
Bruno Rodrigues Marciano <bruno-9237@hotmail.com>
2018-11-11 01:05:16 -0200

    Validação de vitória do jogador ou do Dealer

--- blackjack.py
+++ blackjack.py
@@ -2,7 +2,6 @@ import emoji
 from random import shuffle
 from typing import List
 
-# Classe Carta
 class Carta:
     def __init__(self, valor, naipe):
         self.__valor = valor
@@ -14,26 +13,23 @@ class Carta:
     def __str__(self):
         return f'{self.__valor}{self.__naipe}'
 
-# Classe Dealer
 class Dealer:
-    def __init__(self, nome, sexo):
-        self.nome = nome
-        self.sexo = sexo
+    def __init__(self):
         self.__baralho = self.__criar_baralho()
-        self.descartes = []
+        self.__descartes = []
+        self.__cartas = []
 
     def __embaralhar(self):        
         shuffle(self.__baralho)
 
     def __criar_baralho(self):
+        cartas = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
         naipes = (
             emoji.emojize(':spades:', use_aliases=True), 
             emoji.emojize(':hearts:', use_aliases=True),
             emoji.emojize(':clubs:', use_aliases=True),
             emoji.emojize(':diamonds:', use_aliases=True)
         )
-
-        cartas = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
 
         baralho = []
         for valor in cartas:
@@ -43,37 +39,45 @@ class Dealer:
         return baralho
 
     def iniciar_jogo(self):
-        # Quando houver cartas descartadas, elas deverão
-        # ser devolvidas para o baralho no inicio da rodada
-        if len(self.descartes) > 0:
-            for carta in self.descartes:
+        # Devolver as cartas descartadas no ínicio da rodada
+        if len(self.__descartes) > 0:
+            for carta in self.__descartes:
+                self.__baralho.append(carta)
+
+        # Devolver as cartas do Dealer para o baralho
+        if len(self.__cartas) > 0:
+            for carta in self.__cartas:
                 self.__baralho.append(carta)
         
         self.__embaralhar()
-        for i in range(1, 6):
-            # No inicio da rodada serão descartadas 5 cartas...
-            self.descartes.append(self.__baralho.pop())
+
+        # No inicio da rodada serão descartadas 5 cartas e entregues 2 ao Dealer
+        self.__descartes = [self.__baralho.pop() for _ in range(0, 5)]
+        self.__cartas = [self.__baralho.pop() for _ in range(0, 2)]
+
+    def receber_cartas(self, cartas):
+        for carta in cartas:
+            self.__cartas.append(carta)
 
     def entregar_cartas(self):
-        cartas = [self.__baralho.pop(), self.__baralho.pop()]
-        return cartas
+        return [self.__baralho.pop(), self.__baralho.pop()]
 
     def entregar_carta(self):
         return self.__baralho.pop()
 
-    def __validar_melhor_combinacao_de_as(self, soma, qtd_as):
-        s = 0
-        soma_aux = 0
-        for i in range(0, qtd_as+1):
-            soma_aux = soma + (10 * (qtd_as - i)) + i
+    def somatorio_cartas(self, cartas_jogador: List[Carta]):
 
-            if soma_aux > s and soma_aux <= 21: s = soma_aux
-            elif soma_aux > s and (s > 21 or s == 0): s = soma_aux
-            elif soma_aux < s and s > 21: s = soma_aux
-            
-        return s
+        def somatorio_as(soma, qtd_as):
+            s = 0
+            soma_aux = 0
+            for i in range(0, qtd_as+1):
+                soma_aux = soma + (10 * (qtd_as - i)) + i
 
-    def validar_blackjack(self, cartas_jogador: List[Carta]):
+                if soma_aux > s and soma_aux <= 21: s = soma_aux
+                elif soma_aux > s and (s > 21 or s == 0): s = soma_aux
+                elif soma_aux < s and s > 21: s = soma_aux
+                
+            return s
 
         cartas_as = []
         soma = 0
@@ -83,28 +87,51 @@ class Dealer:
             else: soma += int(carta.valor())
 
         if len(cartas_as) > 0:
-            soma = self.__validar_melhor_combinacao_de_as(soma, len(cartas_as))
+            soma = somatorio_as(soma, len(cartas_as))
 
         return soma
 
-    def __str__(self):
-        return f'Dealer {self.nome}'
+    def quantidade_cartas_baralho(self):
+        return len(self.__baralho)
 
-# Classe jogador
-class Jogador:
-    def __init__(self, nome, sexo):
-        self.nome = nome
-        self.sexo = sexo
-        # Adicionar emoji correspondente
-        if (sexo == 'M'):
-            pass
+    def resultado_jogador(self, jogador):
+        # -1 = Derrota do jogador, 0 = Empate, 1 = Vitória do jogador
+        soma_jogador = self.somatorio_cartas(jogador.cartas())
+        if soma_jogador == 21:
+            return 1
+        elif soma_jogador > 21:
+            return -1
+        elif soma_jogador < 21:
+            soma_dealer = self.somatorio_cartas(self.__cartas)
+            if soma_dealer > soma_jogador:
+                return -1
+            elif soma_dealer < soma_jogador:
+                pode_jogar = True
+                while pode_jogar:
+                    self.__cartas.append(self.entregar_carta())
+                    soma_dealer = self.somatorio_cartas(self.__cartas)
+                    if soma_dealer > 21:
+                        return 1
+                    elif soma_dealer == 21 or soma_dealer > soma_jogador:
+                        return -1
+                    else:
+                        return 0
+            else:
+                return 0
         else:
-            pass
+            return 0
+
+class Jogador:
+    def __init__(self, nome):
+        self.__nome = nome
         self.__cartas = []
 
     def receber_cartas(self, cartas_recebidas):
         for carta in cartas_recebidas:
             self.__cartas.append(carta)
+
+    def nome(self):
+        return self.__nome
 
     def get_cartas_formatada(self):
         fcartas = ''
--- tests.py
+++ tests.py
@@ -1,153 +1,218 @@
-import blackjack as bj
+from blackjack import Dealer, Carta, Jogador
 
-def teste_validar_blackjack():
-    dealer = bj.Dealer('teste', 'F')
+def teste_dealer_quantidade_cartas_baralho():
+    dealer = Dealer('teste', 'M')
+    assert dealer.quantidade_cartas_baralho() == 52
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('9', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus') 
+def teste_dealer_descartes_inicio_jogo():
+    dealer = Dealer('teste', 'M')
+    dealer.iniciar_jogo()
+    assert dealer.quantidade_cartas_baralho() == 45
+
+def teste_somatorio_cartas():
+    dealer = Dealer('teste', 'M')
+
+    assert dealer.somatorio_cartas([ 
+        Carta('9', ''), 
+        Carta('A', ''), 
+        Carta('A', '') 
     ]) == 20
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('7', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus') 
+    assert dealer.somatorio_cartas([ 
+        Carta('7', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', '') 
     ]) == 20
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('7', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus') 
+    assert dealer.somatorio_cartas([ 
+        Carta('7', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', '') 
     ]) == 19
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('7', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus') 
+    assert dealer.somatorio_cartas([ 
+        Carta('7', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', '') 
     ]) == 19
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('8', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus') 
+    assert dealer.somatorio_cartas([ 
+        Carta('8', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', '') 
     ]) == 20
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('9', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('9', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', ''),
+        Carta('A', '')
     ]) == 13
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', ''),
+        Carta('A', '')
     ]) == 14
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('3', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('J', ''), 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('3', '')
     ]) == 15
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('J', ''), 
+        Carta('A', ''), 
+        Carta('A', '')
     ]) == 21
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('A', ''), 
+        Carta('A', ''), 
+        Carta('A', ''),
+        Carta('A', '')
     ]) == 13
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('2', 'paus'), 
-        bj.Carta('5', 'paus'), 
-        bj.Carta('9', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('2', ''), 
+        Carta('5', ''), 
+        Carta('9', ''),
+        Carta('A', '')
     ]) == 17
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('2', 'paus'), 
-        bj.Carta('6', 'paus'), 
-        bj.Carta('9', 'paus'),
-        bj.Carta('4', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('2', ''), 
+        Carta('6', ''), 
+        Carta('9', ''),
+        Carta('4', '')
     ]) == 21
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('A', ''), 
+        Carta('A', '')
     ]) == 20
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus'),
-        bj.Carta('J', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('A', ''),
+        Carta('A', ''),
+        Carta('J', '')
     ]) == 21
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('J', ''), 
+        Carta('A', ''),
+        Carta('A', '')
     ]) == 22
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('J', ''), 
+        Carta('A', ''),
+        Carta('A', ''),
+        Carta('A', '')
     ]) == 23
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('2', 'paus'),
-        bj.Carta('2', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('J', ''), 
+        Carta('2', ''),
+        Carta('2', ''),
+        Carta('A', '')
     ]) == 25
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('2', 'paus'),
-        bj.Carta('2', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('J', ''), 
+        Carta('2', ''),
+        Carta('2', ''),
+        Carta('A', '')
     ]) == 25
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('3', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('J', ''), 
+        Carta('3', ''),
+        Carta('A', '')
     ]) == 24
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('J', 'paus'), 
-        bj.Carta('3', 'paus'),
-        bj.Carta('A', 'paus'),
-        bj.Carta('A', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('J', ''), 
+        Carta('3', ''),
+        Carta('A', ''),
+        Carta('A', '')
     ]) == 25
 
-    assert dealer.validar_blackjack([ 
-        bj.Carta('10', 'paus'), 
-        bj.Carta('A', 'paus'), 
-        bj.Carta('A', 'paus'),
-        bj.Carta('J', 'paus')
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('A', ''), 
+        Carta('A', ''),
+        Carta('J', '')
     ]) == 22
+
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('5', ''), 
+        Carta('2', ''),
+        Carta('3', '')
+    ]) == 20
+
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('5', ''), 
+        Carta('2', ''),
+        Carta('3', ''),
+        Carta('A', '')
+    ]) == 21
+
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('Q', ''), 
+        Carta('A', '')
+    ]) == 21
+
+    assert dealer.somatorio_cartas([ 
+        Carta('10', ''), 
+        Carta('K', ''), 
+        Carta('2', '')
+    ]) == 22 
+
+    assert dealer.somatorio_cartas([ 
+        Carta('J', ''), 
+        Carta('2', '')
+    ]) == 12     
+
+def teste_empate():
+    dealer = Dealer('teste', 'M')
+    jogador = Jogador('teste', 'M')
+
+    jogador.receber_cartas([Carta('A', ''), Carta('A', '')])
+    dealer.receber_cartas([Carta('J', ''), Carta('A', '')])
+    assert dealer.resultado_jogador(jogador) == 0
+
+def teste_vitoria_jogador_com_blackjack():
+    dealer = Dealer('teste', 'M')
+    jogador = Jogador('teste', 'M')
+
+    jogador.receber_cartas([Carta('10', ''), Carta('A', ''), Carta('A', '')])
+    dealer.receber_cartas([Carta('J', ''), Carta('A', '')])
+    assert dealer.resultado_jogador(jogador) == 1      
+
+def teste_derrota_jogador_com_soma_maior_que_21():
+    dealer = Dealer('teste', 'M')
+    jogador = Jogador('teste', 'M')
+
+    jogador.receber_cartas([Carta('A', ''), Carta('A', ''), Carta('10', ''), Carta('10', '')])
+    dealer.receber_cartas([Carta('J', ''), Carta('2', '')])
+    assert dealer.resultado_jogador(jogador) == -1
