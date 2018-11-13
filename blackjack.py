1d579837cab570fe0088f3bc08cb5215f7f72118
Bruno Rodrigues Marciano <bruno-9237@hotmail.com>
2018-11-13 11:03:35 -0200

    Alterado todos os membros de classe para acesso privado

--- blackjack.py
+++ blackjack.py
@@ -4,23 +4,23 @@ from typing import List
 
 class Carta:
     def __init__(self, valor, naipe):
-        self.__valor = valor
-        self.__naipe = naipe
+        self._valor = valor
+        self._naipe = naipe
 
     def valor(self):
-        return self.__valor
+        return self._valor
 
     def __str__(self):
-        return f'{self.__valor}{self.__naipe}'
+        return f'{self._valor}{self._naipe}'
 
 class Dealer:
     def __init__(self):
-        self.__baralho = self.__criar_baralho()
-        self.__descartes = []
-        self.__cartas = []
+        self._baralho = self.__criar_baralho()
+        self._descartes = []
+        self._cartas = []
 
     def __embaralhar(self):        
-        shuffle(self.__baralho)
+        shuffle(self._baralho)
 
     def __criar_baralho(self):
         cartas = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
@@ -40,37 +40,37 @@ class Dealer:
 
     def iniciar_jogo(self):
         # Devolver as cartas descartadas no ínicio da rodada
-        if len(self.__descartes) > 0:
-            for carta in self.__descartes:
-                self.__baralho.append(carta)
+        if len(self._descartes) > 0:
+            for carta in self._descartes:
+                self._baralho.append(carta)
 
         # Devolver as cartas do Dealer para o baralho
-        if len(self.__cartas) > 0:
-            for carta in self.__cartas:
-                self.__baralho.append(carta)
+        if len(self._cartas) > 0:
+            for carta in self._cartas:
+                self._baralho.append(carta)
         
         self.__embaralhar()
 
         # No inicio da rodada serão descartadas 5 cartas e entregues 2 ao Dealer
-        self.__descartes = [self.__baralho.pop() for _ in range(0, 5)]
-        self.__cartas = [self.__baralho.pop() for _ in range(0, 2)]
+        self._descartes = [self._baralho.pop() for _ in range(0, 5)]
+        self._cartas = [self._baralho.pop() for _ in range(0, 2)]
 
     def receber_cartas(self, cartas):
         for carta in cartas:
-            self.__cartas.append(carta)
+            self._cartas.append(carta)
 
     def receber_cartas_jogador(self, cartas):
         for carta in cartas:
-            self.__baralho.append(carta)
+            self._baralho.append(carta)
 
     def entregar_cartas_jogador(self):
         cartas = []
-        cartas.append(self.__baralho.pop())
-        cartas.append(self.__baralho.pop())
+        cartas.append(self._baralho.pop())
+        cartas.append(self._baralho.pop())
         return cartas
 
     def entregar_carta(self):
-        return self.__baralho.pop()
+        return self._baralho.pop()
 
     def somatorio_cartas(self, cartas_jogador: List[Carta]):
 
@@ -105,7 +105,7 @@ class Dealer:
         return soma
 
     def quantidade_cartas_baralho(self):
-        return len(self.__baralho)
+        return len(self._baralho)
 
     def resultado_jogador(self, jogador):
         # -1 = Derrota do jogador, 0 = Empate, 1 = Vitória do jogador
@@ -113,7 +113,7 @@ class Dealer:
         if soma_jogador > 21:
             return -1
         else:
-            soma_dealer = self.somatorio_cartas(self.__cartas)
+            soma_dealer = self.somatorio_cartas(self._cartas)
             if soma_dealer == 21 and soma_jogador == 21:
                 return 0
             elif soma_dealer == 21 or (soma_dealer > soma_jogador and soma_dealer < 21):
@@ -121,8 +121,8 @@ class Dealer:
             elif soma_dealer < soma_jogador:
                 pode_jogar = True
                 while pode_jogar:
-                    self.__cartas.append(self.entregar_carta())
-                    soma_dealer = self.somatorio_cartas(self.__cartas)
+                    self._cartas.append(self.entregar_carta())
+                    soma_dealer = self.somatorio_cartas(self._cartas)
                     if soma_dealer == soma_jogador:
                         return 0
                     elif soma_dealer == 21 or (soma_dealer < 21 and soma_dealer > soma_jogador):
@@ -135,37 +135,37 @@ class Dealer:
                 return 0
 
     def cartas(self):
-        return self.__cartas
+        return self._cartas
 
     def cartas_str(self):
         cartas = ''
-        for carta in self.__cartas:
+        for carta in self._cartas:
             cartas += f'{carta} '
         return cartas
 
 class Jogador:
     def __init__(self, nome):
-        self.__nome = nome
-        self.__cartas = []
+        self._nome = nome
+        self._cartas = []
 
     def receber_cartas(self, cartas_recebidas):
         for carta in cartas_recebidas:
-            self.__cartas.append(carta)
+            self._cartas.append(carta)
 
     def nome(self):
-        return self.__nome
+        return self._nome
 
     def cartas_str(self):
         cartas = ''
-        for carta in self.__cartas:
+        for carta in self._cartas:
             cartas += f'{carta} '
         return cartas
 
     def cartas(self):
-        return self.__cartas
+        return self._cartas
 
     def entregar_cartas(self):
-        return [self.__cartas.pop() for _ in range(0, len(self.__cartas))]    
+        return [self._cartas.pop() for _ in range(0, len(self._cartas))]    
 
     def receber_carta(self, carta_recebida):
-        self.__cartas.append(carta_recebida)
+        self._cartas.append(carta_recebida)
